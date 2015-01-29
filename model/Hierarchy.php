5f06d9eec6182d6e922466e7d78b4cbaae851326
John Milmine <john.milmine@dna.co.nz>
2015-01-29 15:01:36 +1300

    adding hide from hierachy

--- model/Hierarchy.php
+++ model/Hierarchy.php
@@ -37,7 +37,19 @@ class Hierarchy extends DataExtension {
 	 * all children, regardless of this setting.
 	 */
 	private static $node_threshold_leaf = 250;
-	
+
+	/**
+	 * A list of classnames to exclude from display in the hierarchy.
+	 * Especially useful for big sets of pages like listings
+	 * If you use this, and still need the classes to be editable
+	 * then add a model admin for the class
+	 * Note: Does not filter subclasses (non-inheriting)
+	 *
+	 * @var array
+	 * @config
+	 */
+	private static $hide_from_hierarchy = array();
+
 	public function augmentSQL(SQLQuery &$query) {
 	}
 
@@ -619,9 +631,13 @@ class Hierarchy extends DataExtension {
 	 */
 	public function stageChildren($showAll = false) {
 		$baseClass = ClassInfo::baseDataClass($this->owner->class);
+		$hidden = $this->owner->config()->hide_from_hierarchy;
 		$staged = $baseClass::get()
 			->filter('ParentID', (int)$this->owner->ID)
 			->exclude('ID', (int)$this->owner->ID);
+		if (count($hidden) > 0) {
+			$staged->exclude('ClassName', $hidden);
+		}
 		if (!$showAll && $this->owner->db('ShowInMenus')) {
 			$staged = $staged->filter('ShowInMenus', 1);
 		}
@@ -643,6 +659,7 @@ class Hierarchy extends DataExtension {
 		}
 
 		$baseClass = ClassInfo::baseDataClass($this->owner->class);
+		$hidden = $this->owner->config()->hide_from_hierarchy;
 		$children = $baseClass::get()
 			->filter('ParentID', (int)$this->owner->ID)
 			->exclude('ID', (int)$this->owner->ID)
@@ -650,7 +667,9 @@ class Hierarchy extends DataExtension {
 				'Versioned.mode' => $onlyDeletedFromStage ? 'stage_unique' : 'stage',
 				'Versioned.stage' => 'Live'
 			));
-		
+		if (count($hidden) > 0) {
+			$children->exclude('ClassName', $hidden);
+		}
 		if(!$showAll) $children = $children->filter('ShowInMenus', 1);
 
 		return $children;
